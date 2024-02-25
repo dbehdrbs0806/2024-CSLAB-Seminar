ed1bc1b3fe3be301dba6ebe1850617f031cb50aa
dbehdrbs0806 <a67422830@gmail.com>
2024-02-26 00:22:51 +0900

    Gradle 설정 추가

---
+++ HealthGraph/src/main/java/org/example/healthgraph/HealthGraphApplication.java
@@ -0,0 +1,13 @@
+package org.example.healthgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthGraphApplication.class, args);
+    }
+
+}
